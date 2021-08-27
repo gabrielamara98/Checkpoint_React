bc198db83033766d8413359773c170f6a27da67d
Capamara <gabriel.amara98@gmail.com>
2021-08-27 17:45:15 -0300

    "Estrutura base do corpo"

---
+++ src/componentes/Corpo/Corpo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Corpo_1 from '../Corpo_1/Corpo_1'
+import './Corpo.css'
+
+
+export default function Corpo() {
+    return (
+        <main className="post">
+            <Corpo_1/>
+        </main>
+    )
+}
+
