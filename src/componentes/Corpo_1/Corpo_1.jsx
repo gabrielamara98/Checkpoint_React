ba8a1668d2e6b887379ffe5d4648f10193478bbf
Capamara <gabriel.amara98@gmail.com>
2021-08-29 11:37:48 -0300

    "RASCUNHO CORPO 1"

--- src/componentes/Corpo_1/Corpo_1.jsx
+++ src/componentes/Corpo_1/Corpo_1.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Corpo_1.css'
+import author from '../img/author.png'
+
 export default function Corpo_1() {
     return (
+        <>
        <header className="parte1">
-           <h1></h1>
-           
+           <h1 className="post__title">Quais são os sintomas da variante Delta da Covid-19? Teste seus conhecimnetos</h1>
+           <p className="post__excerpt">A infecção causada pela variante Delta pode provocar sintomas diferentes da doença causada pela linhagem original do novo coronavírus</p>
+           <section className="author__content">
+               <figure className="author__image">
+                   <img  className ="author__image"src={author}/>
+               </figure>
+           </section>
+           <div className="higher__share">
+               <span className="post__data">24/08/2021 às 16:44 | Atualizado às 16:56 
+               </span>
+           </div>
        </header>
+       </>
     )
 }
