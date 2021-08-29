9edf70b8afe5dc4d9da61b9ea36db3ae79cff219
Capamara <gabriel.amara98@gmail.com>
2021-08-29 13:57:13 -0300

    "Revisao Final"

--- src/componentes/Corpo_2/Corpo_2.jsx
+++ src/componentes/Corpo_2/Corpo_2.jsx
@@ -11,8 +11,8 @@ export default function Corpo_2() {
                     <img className="custom-logo-ng-scope"src={Logo}/>
                 </figure>
             </div>
-            <p>Por ser uma doença nova, a Covid-19 ainda não foi compreendida totalmente pela comunidade médica global. Os conhecimentos sobre a doença estão em constante atualização, incluindo os sintomas e os impactos para a saúde a curto e longo prazos.</p>
-            <p>A variante Delta da Covid-19, originalmente conhecida como B.1.617.2, existe desde o final do ano passado, mas nos últimos meses tornou-se rapidamente dominante em muitos países. Ainda não está claro o quanto essa cepa é mais transmissível. Estimativas variam entre 60% e 200% mais transmissível, dependendo de quem apresenta a estimativa.</p>
+            <p>Por ser uma doença nova, a <a href="">Covid-19</a> ainda não foi compreendida totalmente pela comunidade médica global. Os conhecimentos sobre a doença estão em constante atualização, incluindo os sintomas e os impactos para a saúde a curto e longo prazos.</p>
+            <p>A <a href=""> variante Delta da Covid-19,</a>originalmente conhecida como B.1.617.2, existe desde o final do ano passado, mas nos últimos meses tornou-se rapidamente dominante em muitos países. Ainda não está claro o quanto essa cepa é mais transmissível. Estimativas variam entre 60% e 200% mais transmissível, dependendo de quem apresenta a estimativa.</p>
         </div>
     )
 }
--- src/componentes/Itens/Itens.jsx
+++ src/componentes/Itens/Itens.jsx
@@ -5,11 +5,12 @@ export default function Itens() {
     return (
         <nav className="content-menu">
             <ul className="menu">
-                <li><a href="">Nacional</a></li>
+                <li><a href=""className="Nacional">Nacional<i class="fas fa-circle"></i></a></li>
                 <li><a href="">Business</a></li>
                 <li><a href="">Internacional</a></li>
-                <li><a href=""><b>Saude</b></a></li>
+                <li><a href=""><b>Saúde</b></a></li>
                 <li><a href="">Tecnologia</a></li>
+                <li><a href="">Esporte</a></li>
                 <li><a href="">Entretenimento</a></li>
                 <li><a href="">Estilo</a></li>
                 <li><a href="">Viagem & Gastronomia</a></li>
